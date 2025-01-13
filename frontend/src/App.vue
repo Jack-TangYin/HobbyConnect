c9250ef1ffe0322357b9b9332db1e943d99b4767
romang2103 <romang2103@gmail.com>
2025-01-13 18:03:34 +0000

    Fixed routing and moved layout.html to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <main class="container pt-4">
         <div>
             <router-link
@@ -10,13 +10,76 @@
             |
             <router-link
                 class=""
-                :to="{name: 'Other Page'}"
+                :to="{name: 'Connect Page'}"
             >
-                Other Page
+                Connect Page
             </router-link>
         </div>
         <RouterView class="flex-shrink-0" />
     </main>
+</template> -->
+<template>
+    <div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <router-link class="navbar-brand" :to="{name: 'Main Page'}">Main Page</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Main navigation items on the left -->
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Main Page'}">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Connect Page'}">Connect</router-link>
+          </li>
+          
+        </ul>
+
+        <!-- Right-aligned items with 'Jack' touching the edge -->
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item" style="position: relative; right: -15px;">
+            <a class="nav-link active" aria-current="page">
+              <!-- {{user.username}} -->
+               Username
+            </a>
+          </li> 
+          <li class="nav-item dropdown" style="position: relative; bottom: -2px;">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+            </a>
+            <!-- Aligns dropdown menu within viewport -->
+            <!-- <ul class="dropdown-menu dropdown-menu-end">
+              <li><router-link class="dropdown-item" to="{name: 'Profile Page'}">Profile</router-link></li>
+              <li><a class="dropdown-item" href="#">Settings</a>
+            <router-link class="dropdown-item" to="{name: 'Settings Page'}">Settings</router-link></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><router-link class="dropdown-item" to="{name: 'Login Page'}">Sign Out</router-link></li>
+            </ul> -->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <RouterView class="flex-shrink-0" />
+  </div>
+</div>
 </template>
 
 <script lang="ts">
